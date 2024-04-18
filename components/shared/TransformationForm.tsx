3c7c45d5423010ee8d796c8c71b02527151de2b8
Anita <jelicanita7@gmail.com>
2024-04-18 19:31:28 +0200

    Transformed Image Components & Server actions
    Add image, update image, delete image, get image

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -30,6 +30,7 @@ import { useState, useTransition } from "react"
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils"
 import { updateCredits } from "@/lib/actions/user.actions"
 import MediaUploader from "./MediaUploader"
+import TransformedImage from "./TransformedImage"
 
 export const formSchema = z.object({
   title: z.string(),
@@ -106,7 +107,7 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
         setNewTransformation(null);
 
         startTransition(async () => {
-            // await updateCredits(userId, creditFee)
+            await updateCredits(userId, -1)
         })
     }
 
@@ -210,6 +211,15 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
                         type={type}
                     />
                 )}
+            />
+
+            <TransformedImage
+                image={image}
+                type={type}
+                title={form.getValues().title}
+                isTransforming={isTransforming}
+                setIsTransforming={setIsTransforming}
+                transformationConfig={transformationConfig}
             />
         </div>
 
